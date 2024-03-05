71c593ceac03baab6411b6bb9ff0b74f9dcbb571
2022102139 <2022102139@LAB1-006>
2024-03-05 20:43:01 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01
+{
+    public class Calculadora
+    {
+        public int Soma(int num1, int num2)
+
+        { 
+            return num1 + num2; 
+        }
+       
+
+        
+    }
+}
+
---
+++ Aula01/Program.cs
@@ -0,0 +1,43 @@
+﻿//altere o programa para calcular                         No "Do while", o codigo sera executado ao menos 1 vez indepedente de ser true ou false.
+
+// imc = peso / altura * altura
+using Aula01;
+using System.Globalization;
+
+bool entrada = true;
+do
+{
+    try
+    {
+        Console.WriteLine("Informe o primero numero: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe o segundo numero: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Calculadora calculadora = new Calculadora();
+        Console.WriteLine($"A soma dos numeros {num1} + {num2} = {calculadora.Soma(num1, num2)}");
+       entrada = false;
+    }
+    catch (FormatException e)
+    {
+        
+    }
+} while (entrada);
+
+/*bool entrada = true;
+do
+{
+    try
+    {
+        Console.WriteLine("Informe o peso atual: ");
+        int peso = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe a altura atual: ");
+        float altura = float.Parse(Console.ReadLine());
+        entrada = false;
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("O peso deve ser um número inteiro e a altura em numero decimal");
+    }
+} while (entrada);
+Crie uma classe chamada calculadora, que possua um método que some dois numeros inteiros e retorne o resultado dessa soma. O nome do método deve ser soma;
+
